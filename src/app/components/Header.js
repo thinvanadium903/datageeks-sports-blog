8b05e97da8be8b6375fe43b1d2e8b37bdbfc18d0
thinvanadium903 <ppb93083@uga.edu>
2024-10-07 12:00:46 -0400

    im having an internal debate over whether to use this "grand" container for centering... may experiment later

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,13 +5,15 @@ import Link from "next/link";
 
 function Header() {
     return (
-        <div className="header">
-            <div id="about"><Link href='/about'>About This Site</Link></div>
-            <div id="title">
-                <p><Link href='/'>DataGeeks Sports Blog</Link></p>
+        <div id='grand'>
+            <div className="header">
+                <div id="about"><Link href='/about'>About This Site</Link></div>
+                <div id="title">
+                    <p><Link href='/'>DataGeeks Sports Blog</Link></p>
+                </div>
+                <div id="login"><Link href='/login'>Log In</Link></div>
+                
             </div>
-            <div id="login"><Link href='/login'>Log In</Link></div>
-            
         </div>
     )
 }
