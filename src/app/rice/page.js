3bcff38bea9808b228b92c38893764ebf02ae718
thinvanadium903 <ppb93083@uga.edu>
2024-10-08 08:42:09 -0400

    various style edits

--- src/app/rice/page.js
+++ src/app/rice/page.js
@@ -12,20 +12,22 @@ function App() {
       <div className='full'>
         <h1>The Unparalleled Career Dominance of Jerry Rice</h1>
         <div id='lead_image'></div>
-        <p>In sports, there are many arguments - rational and otherwise - that fans and observers use to gauge
-              who is the greatest player of all time in a particular sport or category. The most common arguments seem to balance a player's 
-              peak performance, longevity, consistency, playoff performance, team success, and individual talent/athleticism.
+        <p>Welcome to DataGeeks Sports Blog! I've had the idea to make a sports blog for a while, to showcase and highlight 
+          some of the most fascinating sports stories and facts out there. Unfortunately at the time, I had no 
+          website creation skills, so I was forced to shelve the project. However, I've kept a list of interesting topics that I want 
+          to cover, and this is the first one on that list - the insanity of Jerry Rice's career.
         </p>
-        <p>These are subjective criteria; there is no "formula" to plug in a player's numbers and spit out an assessment of that player's career. 
-          And in sports, two fans may have drastically different interpretations of what makes a player the "greatest of all time." When judging 
-          quarterbacks, some people value rings and team success over statistics and would name Tom Brady as the G.O.A.T.. Are you a numbers person that values regular season consistency? 
-          Peyton Manning is your guy. Is clutch playoff performance the thing you value most? Well, Patrick Mahomes is the leader in postseason passer rating. 
-          Point being, almost no one player in any sports conversation will satisfy all criteria set forth by an at-home analyst.
+        <p>In sports, there are many arguments that fans and observers use to gauge
+              who is the greatest player of all time in a particular sport or category. Most arguments seem to balance a player's 
+              peak performance, longevity, consistency, playoff performance, team success, and the "intangibles." These are subjective criteria; there is no "formula" to plug in a player's numbers and spit out an assessment of that player's career. 
+          And in sports, two fans may have drastically different interpretations of what makes a player the "greatest of all time." And besides, 
+          almost no one player in any sports conversation will satisfy all criteria set forth by an at-home analyst, so these types of conversations usually 
+          produce a variety of names.
         </p>
           <p>However, in football, the wide receiver position does not experience this dilemma; simply put, Jerry Rice has it all. In his unprecedented 20-year
           career, Rice combined elite, league-leading performance with longevity and durability that would be exceptional for a quarterback or kicker,
-          let alone the brutally physical wide receiver position. His career statistics tower over every other receiver's <h4>regardless </h4>
-          of position, and the degree to which he dominated his position makes him arguably the greatest player in NFL history.
+          let alone the brutally physical wide receiver position. His career statistics tower over every other receiver's <h4>despite </h4>
+          playing in a much more defense-friendly era, and the degree to which he dominated his position makes him arguably the greatest player in NFL history.
         </p>
         <h2>The Career Records</h2>
         <p>Jerry Rice doesn't just hold the career records for the major receiving categories like receiving yards and touchdowns - it's 
