fbec34d5afee7f38308038d3c08702a3ab133ecb
thinvanadium903 <ppb93083@uga.edu>
2024-11-29 11:26:56 -0500

    article changes, remove excel file there for some reason

--- src/app/white_sox/page.js
+++ src/app/white_sox/page.js
@@ -14,8 +14,8 @@ function App() {
               from now on. It's called a <em>stub</em>, and it's essentially an article 
               about a neat stat/factoid, but not something suitable or substantial enough for a 
               whole feature-length article. In other words, it's the 'Shorts' edition of YouTube 
-              for written content. Now that I've explained that, let's dive into one of the worst 
-              baseball seasons in history.
+              for written content. Now that I've explained that, let's dive into what the White 
+              Sox have managed to accomplish this season.
             </p>
             <p>
               Yesterday on September 27th, the Chicago White Sox lost to the Detroit Tigers by a 
@@ -23,9 +23,36 @@ function App() {
               extra significance behind it.
             </p>
             <p>
-              After this game, the Chicago White Sox sat at a 40-121 record. Their 121 losses  
-              have now given them the distinction of being THE single best MLB team in modern 
-              history at losing games.
+              This season, the Chicago White Sox had a... trying year - in fact, they've been 
+              struggling as a franchise for several years now. They've been in the ever-tedious 
+              process of rebuilding a team with solid veterans despite not having a large 
+              payroll. Almost all teams do this at some point or another - here's the typical 
+              process.
+            </p>
+            <ol>
+              <li>Team has many good players, wins a lot of games, enjoys success</li>
+              <li>Eventually players retire, or are traded, or sign elsewhere after becoming 
+                too expensive
+              </li>
+              <li>The roster experiences a drop in skill, experience, leadership, and consistency</li>
+              <li>Team regresses from good to bad - revenue declines as ticket and merch sales 
+                stagnate
+              </li>
+              <li>Eventually after several bad years, one of several things happens:</li>
+              <ul>
+                <li>New owner or manager is hired to turn the team around through new coaches, 
+                  free agency signings, and trades
+                </li>
+                <li>Team acquires high draft picks as compensation for poor regular season play, 
+                  and the team builds around those players</li>
+                <li>Team does these two things but continues to be mediocre</li>
+              </ul>
+            </ol>
+            <p>
+              The White Sox are all too familiar with this. In 2021, the last season they made the 
+              playoffs, they did so in a fairly straightforward, no-nonsense way. They built their 
+              roster around an assortment of low-ceiling, high-floor pitchers that could keep the 
+              opponent's score down, and give the middling offense a chance to win games.
             </p>
         </div>
     </div>
